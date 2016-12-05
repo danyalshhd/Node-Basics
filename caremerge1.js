2a39cd44d815abbed33bc1f4fb2bba2ba9e514e4
danyalshhd <dshahid@ssidecisions.com>
2016-12-05 19:50:48 +0500

    'error-handling-1stquestion'

--- caremerge1.js
+++ caremerge1.js
@@ -2,87 +2,93 @@ var express = require('express');
 var http = require("http");
 
 var app = express();
-var counterForWritingEnd = 0;
+
 
 app.get("/I/want/title/", function (request,response) {
 
-	counterForWritingEnd = 0;
+	var counterForWritingEnd = 0;
 	var requestUrl = request.url;
 	writeHeader(response);
-		
+
+	if(requestUrl.indexOf("address") == -1)
+	{
+		writeAddressInUrl(response);
+		return;
+	}
+
 	if(requestUrl.indexOf('&') > -1)
 	{
-		queryStringCount  = Object.keys(request.query.address).length;
-		writeHeader(response);
-		writeTitleHeader(response);
-
-	  	for(var counter = 0;counter < queryStringCount;counter++)
+		var queryStringCount = getQueryStringCount(requestUrl,Object.keys(request.query.address).length);
+		
+		if(queryStringCount == 1)
 		{
-			var splitUrl = request.query.address[counter].split("/");
-			var validateQueryString = splitUrl[0].indexOf(".com") !== -1
+			getSingleWebsiteTitle(request,response);
+		}
+		else
+		{
+			//get multi website title
+			writeHeader(response);
+			writeTitleHeader(response);
 
-			if(validateQueryString)
+			for(var counter = 0;counter < queryStringCount;counter++)
 			{
-				var urlOpts = {host: splitUrl[0], path: splitUrl[1] == undefined ? "/": "/"+splitUrl[1] + "/", port: '80'};
-				var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
+				var splitUrl = request.query.address[counter].split("/");
 
-				var requestForError = http.get(urlOpts, function (res) {
-					
-					res.on('end',function(){
-						counterForWritingEnd++;
-					
-						if(counterForWritingEnd == queryStringCount)
-						{
-							writeTitleFooter(response);
-							writeFooter(response);
-						}
-					}),
-				    res.on('data', function (chunk) {
+				var validateQueryString = splitUrl[0].indexOf(".com") !== -1
+				
+				if(validateQueryString)
+				{
+					var urlOpts = {host: splitUrl[0], path: splitUrl[1] == undefined ? "/": "/"+splitUrl[1] + "/", port: '80'};
+					var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
 
-				        var str=chunk.toString();
-				        var match = re.exec(str);
-				        if (match && match[2]) {
-							
-							writeTitle(response,match[2]);
-				        }
-			
-				    });
-				}).on('error',function(e){
-					console.log("Got error: " + e.message);
-				});
+					var requestForError = http.get(urlOpts, function (res) {
+
+						res.on('end',function(){
+							counterForWritingEnd++;
+
+							if(counterForWritingEnd == queryStringCount)
+							{
+								writeTitleFooter(response);
+								writeFooter(response);
+							}
+						}),
+						res.on('data', function (chunk) {
+
+							try
+							{
+								var str=chunk.toString();
+								var match = re.exec(str);
+								if (match && match[2]) {
+
+									writeTitle(response,match[2]);
+								}
+							}
+							catch(e)
+							{
+								console.log(e);
+							}
+
+						});
+					}).on('error',function(e){
+						console.log("Got error: " + e.message);
+						writeError(response);
+					});
+				}
+				else
+				{
+					writeError(response);
+					break;
+				}
 			}
 		}
 
 	}
 	else
 	{
-		var queryStringUrl = request.query.address;
-		var validateQueryString = queryStringUrl.indexOf(".com") !== -1
-		if(validateQueryString)
-		{
-			var urlOpts = {host: queryStringUrl, path: "/", port: '80'};
-			var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
-			http.get(urlOpts, function (res) {
-		    res.on('data', function (chunk) {
-
-		        var str=chunk.toString();
-		        var match = re.exec(str);
-		        if (match && match[2]) {
-
-					writeTitleHeader(response);
-					writeTitle(response,match[2]);
-					writeTitleFooter(response);
-					writeFooter(response);
-		        }
-		    });
-			}).on("error", function(e){
-			  	console.log("Got error: " + e.message);
-			});
-
-		}
+		getSingleWebsiteTitle(request,response);
 	}
 
-	
+
 });
 
 app.get("*", function (request,response) {
@@ -93,31 +99,124 @@ app.listen(8080);
 
 function writeHeader(response)
 {
-	  response.write("<html>");
-	  response.write("<head><title>Caremerge");
-	  response.write("</title></head>");
-	  response.write("<body>");
+	if(!response.finished)
+	{
+		response.write("<html>");
+		response.write("<head><title>Caremerge");
+		response.write("</title></head>");
+		response.write("<body>");
+	}
 }
 
 function writeFooter(response)
 {
-	response.write("</body>");
-	response.write("</html>");	
-	response.end();
+	if(!response.finished)
+	{
+		response.write("</body>");
+		response.write("</html>");	
+		response.end();
+	}
 }
 
 function writeTitleHeader(response)
 {
-	response.write("<h1> Following are the titles of given websites: </h1>");
-	response.write("<ul>")
+	if(!response.finished)
+	{
+		response.write("<h1> Following are the titles of given websites: </h1>");
+		response.write("<ul>")
+	}
 }
 
 function writeTitleFooter(response)
 {
-	response.write("</ul>")
+	if(!response.finished)
+	{
+		response.write("</ul>")
+	}
 }
 
 function writeTitle(response,title)
 {
-	response.write("<li>" + title + "</li>");
+	if(!response.finished)
+	{
+		response.write("<li>" + title + "</li>");
+	}
+}
+
+function writeError(response)
+{	
+	if(!response.finished)
+	{
+		response.write("<h1>Invalid URL</h1>")
+		writeFooter(response);
+		response.end();
+	}
+}
+
+function writeAddressInUrl(response)
+{
+	if(!response.finished)
+	{
+		response.write("<h1>Please write address in URL so as to get titles</h1>")
+		writeFooter(response);
+		response.end();
+	}	
+}
+
+function getQueryStringCount(url,queryStringCount)
+{
+	var splitUrl = url.split("&");
+
+	var lengthQueryString = 0;
+	if(splitUrl.length == 2 && queryStringCount > 5)
+	{
+		lengthQueryString = 1;
+	}
+	else
+	{
+		lengthQueryString = queryStringCount;
+	}
+
+	return lengthQueryString;
+}
+
+
+function getSingleWebsiteTitle(request,response)
+{
+	var queryStringUrl = request.query.address;
+	var validateQueryString = queryStringUrl.indexOf(".com") !== -1
+	if(validateQueryString)
+	{
+		var urlOpts = {host: queryStringUrl, path: "/", port: '80'};
+		var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
+		http.get(urlOpts, function (res) {
+			res.on('data', function (chunk) {
+				try
+				{
+					var str=chunk.toString();
+					var match = re.exec(str);
+					if (match && match[2]) {
+
+						writeTitleHeader(response);
+						writeTitle(response,match[2]);
+						writeTitleFooter(response);
+						writeFooter(response);
+					}
+				}
+				catch(e)
+				{
+					console.log(e);
+				}
+			});
+		}).on("error", function(e){
+			console.log("Got error: " + e.message);
+			writeError(response);
+		});
+
+	}
+	else
+	{
+		//Invalid URL
+		writeError(response);
+	}
 }
