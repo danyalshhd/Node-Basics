ef25961b6341e25fbba9f0d3b7fe54e947f32877
danyalshhd <dshahid@ssidecisions.com>
2016-12-06 01:20:57 +0500

    'error-handling'

--- caremerge3.js
+++ caremerge3.js
@@ -35,40 +35,79 @@ app.get("/I/want/title/", function (request,response) {
 	var requestUrl = request.url;
 	writeHeader(response);
 
+	if(requestUrl.indexOf("address") == -1)
+	{
+		writeAddressInUrl(response);
+		return;
+	}
+
 	counterForWritingEnd = 0;
 	//For querystrings which contains '&'
 	if(requestUrl.indexOf('&') > -1)
 	{
-		queryStringCount  = Object.keys(request.query.address).length;
+		queryStringCount  = getQueryStringCount(requestUrl,Object.keys(request.query.address).length);
 		
-		writeTitleHeader(response);
-
-		for(var counter = 0;counter < queryStringCount;counter++)
+		if(queryStringCount == 1)
 		{
-			var urlToShow = request.query.address[counter]; 
-			var splitUrl = request.query.address[counter].split("/");
-			var validateQueryString = splitUrl[0].indexOf(".com") !== -1
-
+			var queryStringUrl = request.query.address;
+			var validateQueryString = queryStringUrl.indexOf(".com") !== -1
 			if(validateQueryString)
 			{
-				var urlOpts = {host: splitUrl[0], path: splitUrl[1] == undefined ? "/": "/"+splitUrl[1] + "/", port: '80'};
+				var urlOpts = {host: queryStringUrl, path: "/", port: '80'};
 
-				callbackClosure(urlToShow,function(x2){
-					return getTitle(urlOpts).then(function(responseText){
-						//console.log(responseText + "----then");
-						writeTitle(response,responseText + "--" + x2);
-						counterForWritingEnd++;
-						
-						if(counterForWritingEnd == queryStringCount)
-						{
-							writeTitleFooter(response);
-							writeFooter(response);
-						}
-					}).catch(function(error){
-						console.log(error);
-					});	
+				getTitle(urlOpts).then(function(responseText)
+				{
+					writeTitleHeader(response);
+					writeTitle(response,responseText + "--" + queryStringUrl);
+					writeTitleFooter(response);
+					writeFooter(response);
+
+				}).catch(function(error)
+				{
+					console.log(error);
 				});
-				
+			}
+			else
+			{
+				writeError(response);
+			}
+		}	
+		else
+		{
+			writeTitleHeader(response);
+
+			for(var counter = 0;counter < queryStringCount;counter++)
+			{
+				var urlToShow = request.query.address[counter]; 
+				var splitUrl = request.query.address[counter].split("/");
+				var validateQueryString = splitUrl[0].indexOf(".com") !== -1
+
+				if(validateQueryString)
+				{
+					var urlOpts = {host: splitUrl[0], path: splitUrl[1] == undefined ? "/": "/"+splitUrl[1] + "/", port: '80'};
+
+					callbackClosure(urlToShow,function(x2){
+						return getTitle(urlOpts).then(function(responseText){
+							//console.log(responseText + "----then");
+							writeTitle(response,responseText + "--" + x2);
+							counterForWritingEnd++;
+							
+							if(counterForWritingEnd == queryStringCount)
+							{
+								writeTitleFooter(response);
+								writeFooter(response);
+							}
+						}).catch(function(error){
+							console.log(error);
+						});	
+					});
+					
+				}
+				else
+				{
+					writeError(response);
+					break;
+				}
 			}
 		}
 
@@ -94,6 +133,10 @@ app.get("/I/want/title/", function (request,response) {
 				console.log(error);
 			});
 		}
+		else
+		{
+			writeError(response);
+		}
 	}
 });
 
@@ -102,40 +145,92 @@ app.get("*", function (request,response) {
 	response.status(404).send('Not found');
 });
 
+app.listen(8080);
+
 function callbackClosure(i, callback) 
 {
 	return callback(i);
 }
 
-app.listen(8080);
+function getQueryStringCount(url,queryStringCount)
+{
+	var splitUrl = url.split("&");
+
+	var lengthQueryString = 0;
+	if(splitUrl.length == 2 && queryStringCount > 5)
+	{
+		lengthQueryString = 1;
+	}
+	else
+	{
+		lengthQueryString = queryStringCount;
+	}
+
+	return lengthQueryString;
+}
 
 function writeHeader(response)
 {
-	response.write("<html>");
-	response.write("<head><title>Caremerge");
-	response.write("</title></head>");
-	response.write("<body>");
+	if(!response.finished)
+	{
+		response.write("<html>");
+		response.write("<head><title>Caremerge");
+		response.write("</title></head>");
+		response.write("<body>");
+	}
 }
 
 function writeFooter(response)
 {
-	response.write("</body>");
-	response.write("</html>");	
-	response.end();
+	if(!response.finished)
+	{
+		response.write("</body>");
+		response.write("</html>");	
+		response.end();
+	}
 }
 
 function writeTitleHeader(response)
 {
-	response.write("<h1> Following are the titles of given websites: </h1>");
-	response.write("<ul>")
+	if(!response.finished)
+	{
+		response.write("<h1> Following are the titles of given websites: </h1>");
+		response.write("<ul>")
+	}
 }
 
 function writeTitleFooter(response)
 {
-	response.write("</ul>")
+	if(!response.finished)
+	{
+		response.write("</ul>")
+	}
 }
 
 function writeTitle(response,title)
 {
-	response.write("<li>" + title + "</li>");
+	if(!response.finished)
+	{
+		response.write("<li>" + title + "</li>");
+	}
+}
+
+function writeAddressInUrl(response)
+{
+	if(!response.finished)
+	{
+		response.write("<h1>Please write address in URL so as to get titles</h1>")
+		writeFooter(response);
+		response.end();
+	}	
+}
+
+function writeError(response)
+{	
+	if(!response.finished)
+	{
+		response.write("<h1>Invalid URL</h1>")
+		writeFooter(response);
+		response.end();
+	}
 }
